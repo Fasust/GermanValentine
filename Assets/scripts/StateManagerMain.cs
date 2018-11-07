7b56f0d088cc5f3ea8e665c2db101bbdc8fd5398
Fasust <sebastianf333@googlemail.com>
2018-11-07 17:37:05 +0100

    Background Music

---
+++ Assets/scripts/StateManagerMain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManagerMain : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		FindObjectOfType<AudioManager>().play("Background");
+	}
+
+}
