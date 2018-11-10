963e0c490337a9767d8e99dc50e3662d739c4e11
Fasust <sebastianf333@googlemail.com>
2018-11-10 03:05:11 +0100

    Parallax background + 2 Camera Options + Background Graphics

---
+++ Assets/scripts/SmoothCamera2D.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamera2D : MonoBehaviour
+{
+
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform target;
+	public Camera camera;
+	public Vector3 offset;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (target)
+		{
+			Vector3 point = camera.WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination + offset, ref velocity, dampTime);
+		}
+
+	}
+}
+ 
