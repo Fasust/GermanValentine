cff751f828567befe49509a8f4ab300267af25c7
Fasust <sebastianf333@googlemail.com>
2018-11-28 15:44:18 +0100

    Lever

---
+++ Assets/scripts/leverControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leverControl : MonoBehaviour {
+
+	public swipe swipeControl;
+	public float yLimit;
+	public float upSpeed;
+	public float downSpeed;
+
+	public bool wasHit = false;
+
+	private bool movingUp = false;
+	private bool movingDown = false;
+	private float restingY;
+
+	// Use this for initialization
+	void Start () {
+		restingY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (swipeControl.swipeUp && !movingDown &&!movingUp)
+		{
+			movingUp = true;
+			wasHit = true;
+			FindObjectOfType<AudioManager>().play("LeverUp");
+		}
+
+		if (movingUp)
+		{
+			if(transform.position.y < yLimit)
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y + upSpeed * Time.deltaTime, transform.position.z);
+
+			}
+			else
+			{
+				transform.position = new Vector3(transform.position.x, yLimit, transform.position.z);
+
+				movingUp = false;
+				movingDown = true;
+				FindObjectOfType<AudioManager>().play("LeverHit");
+			}
+			
+		}
+		if (movingDown)
+		{
+			if(transform.position.y > restingY)
+			{
+				transform.position = new Vector3(transform.position.x, transform.position.y - downSpeed * Time.deltaTime, transform.position.z);
+
+			}
+			else
+			{
+				movingDown = false;
+				transform.position = new Vector3(transform.position.x, restingY, transform.position.z);
+
+			}
+		}
+
+	}
+}
--- Assets/scripts/swipe.cs
+++ Assets/scripts/swipe.cs
@@ -4,13 +4,102 @@ using UnityEngine;
 
 public class swipe : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public bool tap, swipeUp, swipeDown, swipeRight, swipeLeft;
+	public Vector2 startTouche, swipeDelta;
+	public int minSwpieDistance = 125;
+
+	private void Update()
+	{
+		//Resets
+		tap = false;
+		swipeUp = false;
+		swipeDown = false;
+		swipeRight = false;
+		swipeLeft = false;
+
+		#region Destop Inputs
+		if (Input.GetMouseButtonDown(0))
+		{
+			tap = true;
+			startTouche = Input.mousePosition;
+
+		}else if (Input.GetMouseButtonUp(0))
+		{
+			Reset();
+		}
+		#endregion
+		#region Mobile Inputs
+		if(Input.touches.Length > 0)
+		{
+			if(Input.touches[0].phase == TouchPhase.Began)
+			{
+				tap = true;
+				startTouche = Input.touches[0].position;
+
+			}else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+			{
+				Reset();
+			}
+		}
+		#endregion
+
+		//Calculate Swipe Distance
+		swipeDelta = Vector2.zero;
+		if(startTouche != Vector2.zero)
+		{
+			if(Input.touches.Length > 0)
+			{
+				swipeDelta = Input.touches[0].position - startTouche;
+			}else if (Input.GetMouseButton(0))
+			{
+				swipeDelta = (Vector2) Input.mousePosition - startTouche;
+			}
+		}
+
+		//Cross Swipe Bounds
+		if(swipeDelta.magnitude > minSwpieDistance)
+		{
+			//Swiped
+			float x = swipeDelta.x;
+			float y = swipeDelta.y;
+
+			if(Mathf.Abs(x) > Mathf.Abs(y))
+			{
+				//Left or Right
+				if(x < 0)
+				{
+					//Left
+					swipeLeft = true;
+				}
+				else
+				{
+					//Right
+					swipeRight = true;
+				}
+			}
+			else
+			{
+				//Up or Down
+				if(y < 0)
+				{
+					//Down
+					swipeDown = true;
+				}
+				else
+				{
+					//Up
+					swipeUp = true;
+				}
+			}
+
+			Reset();
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private void Reset()
+	{
+		startTouche = Vector2.zero;
+		swipeDelta = Vector2.zero;
+
 	}
 }
