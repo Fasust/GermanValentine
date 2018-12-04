b3ddce5acfdba2b0cb1bc70da6358bc7ce3117e2
Fasust <sebastianf333@googlemail.com>
2018-12-04 15:30:13 +0100

    Fact Collection

--- Assets/FactCard.cs
+++ Assets/FactCard.cs
@@ -10,13 +10,26 @@ public class FactCard : MonoBehaviour {
 	public Image loadingAnimation;
 	public TextMeshProUGUI text;
 	public TextMeshProUGUI titelText;
-	public float delay;
+	public float delay = 6;
+	public bool showPermanent = false;
 	private float currentDelay;
 	private bool showing;
 
 	public UnityEvent afterDisplay;
 
-	public string[] facts;
+	public string[] facts = {
+		"\"schandMaien\" are a variant of \"LiebesMaien\" that People Attach to to houses of people they dislike. \n\n" +
+			"They are traditionally decorated with tampons and black ribbons.",
+		"One month after placing there tree, the german bealors come back to the houses of their crushes and put down their tree. \n\n" +
+			"Their efforts will be rewarded with a cake or a beer.",
+		"Every Leap year, the gender roles of the original tradition switch. The girls go out to steal the trees and the boys eagerly lay in their beds. ",
+		"Legally Buying a tree for this tradition is looked down upon. \n\n" +
+			"Thusly Security in german forests on the 1. Of May is on High Alert. ",
+		"Usually the whole process of stealing, dressing and placing the trees is planed days in advance to the first of May. \n\n" +
+			"Beer (as you would expect from Germany) plays a big role in this. ",
+		"Many Bachelors try to avoid the hassle of stealing a tree in the forest by stealing trees that other bachelors did not properly secure on/at the houses of their crushes. ",
+		"The \"Libesmaies\" are usually taped, tied or thrown on to the roofs of the german beachloretts to make them less accessible for potential thieves."
+	};
 	private int index;
 
 	void Start () {
@@ -28,6 +41,11 @@ public class FactCard : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (showPermanent)
+		{
+			return;
+		}
+
 		if (showing)
 		{
 			if(delay > currentDelay)
@@ -52,8 +70,29 @@ public class FactCard : MonoBehaviour {
 		GetComponent<Image>().color = Color.white;
 		loadingAnimation.enabled = true;
 		
-		Debug.Log(facts[index].Replace("\\n","\n"));
 		text.text = facts[index].Replace("\\n", "\n");
-		titelText.text = "Fact #00" + (index+1);
+		titelText.text = "Fact #00" + ((index) + 1);
+	}
+	public void hide()
+	{
+		showing = false;
+
+		text.enabled = false;
+		titelText.enabled = false;
+		GetComponent<Image>().enabled = false;
+		loadingAnimation.enabled = false;
+	}
+	public void showNext()
+	{
+		if (!showing)
+		{
+			show();
+		}
+		index++;
+		index = index % facts.Length;
+
+		text.text = facts[index ].Replace("\\n", "\n");
+		titelText.text = "Fact #00" + (index+ 1);
+
 	}
 }
