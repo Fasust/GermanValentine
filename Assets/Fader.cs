657634ee90fe5438a69f98f122f6e3898f334e20
Fasust <sebastianf333@googlemail.com>
2018-12-06 15:15:12 +0100

    Tutoring Feedback

---
+++ Assets/Fader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fader : MonoBehaviour {
+
+	public Image image;
+	public float minTranperancy;
+	public float speed;
+
+	private float currentTransperncy = 1;
+	
+	// Update is called once per frame
+	void Update () {
+		if( currentTransperncy > minTranperancy)
+		{
+			Color color = new Color(1, 1, 1, currentTransperncy);
+		}
+	}
+}
