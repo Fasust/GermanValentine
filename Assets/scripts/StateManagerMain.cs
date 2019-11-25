7be77b8eb985fbebe7acc14046e060441e19c2d4
Fasust <sebastianf333@googlemail.com>
2019-11-25 20:10:04 +0100

    new buttons

--- Assets/scripts/StateManagerMain.cs
+++ Assets/scripts/StateManagerMain.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StateManagerMain : MonoBehaviour {
     public Button replayButton;
+    public Button startOverButton;
     public bool hideReplay = true;
     void Start() {
 
-        //Buttons -------------------
         if (hideReplay) {
             replayButton.enabled = false;
             replayButton.image.enabled = false;
+            replayButton.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
+
+            startOverButton.enabled = false;
+            startOverButton.image.enabled = false;
+            startOverButton.GetComponentInChildren<TextMeshProUGUI>().enabled = false;
         }
 
     }
@@ -22,9 +28,17 @@ public class StateManagerMain : MonoBehaviour {
     public void showReplay() {
         replayButton.enabled = true;
         replayButton.image.enabled = true;
+        replayButton.GetComponentInChildren<TextMeshProUGUI>().enabled = true;
+
+        startOverButton.enabled = true;
+        startOverButton.image.enabled = true;
+        startOverButton.GetComponentInChildren<TextMeshProUGUI>().enabled = true;
     }
 
     public void loadLevel(string name) {
         SceneManager.LoadScene(name, LoadSceneMode.Single);
+    }
+    public void startOver() {
+        loadLevel("chop");
     }
 }
