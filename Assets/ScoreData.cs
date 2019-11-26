a4b776985c3099fd20a152f9d40a8a32e95042a9
Fasust <sebastianf333@googlemail.com>
2019-11-26 15:35:30 +0100

    score display

---
+++ Assets/ScoreData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreData {
+    private const int SCORE_NUMBER_LENGTH = 6;
+    public int score;
+    public string name;
+    public ScoreData(string input) {
+        score = int.Parse(input.Substring(0, SCORE_NUMBER_LENGTH - 1));
+        name = input.Substring(SCORE_NUMBER_LENGTH, input.Length - SCORE_NUMBER_LENGTH);
+    }
+    public ScoreData(int score, string name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    public string ToString() {
+        String scoreString = getFormatedScore();
+        return scoreString + name;
+    }
+    public String getFormatedScore() {
+        double scoreDigits = Math.Floor(Math.Log10(score) + 1);
+        String scoreString = score.ToString();
+        int neededDigits = SCORE_NUMBER_LENGTH - (int)scoreDigits;
+
+        for (int i = 0; i < neededDigits; i++) {
+            scoreString = "0" + scoreString;
+        }
+        return scoreString;
+    }
+
+}
---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreDisplay : MonoBehaviour {
+    private string PrefKeyPrefix = "german_valentine_score_";
+    private string PrefKeySize = "german_valentine_score_size";
+    private TextMeshProUGUI display;
+
+    private List<ScoreData> scores = new List<ScoreData>();
+    void Start() {
+        display = GetComponent<TextMeshProUGUI>();
+
+        ScoreData dummy = new ScoreData(1002, "Fasust");
+        PlayerPrefs.SetInt(PrefKeySize, 1);
+        PlayerPrefs.SetString(PrefKeyPrefix + 0.ToString(), dummy.ToString());
+
+        loadScores();
+        sortScores();
+        fillDisplay();
+    }
+
+    private void sortScores() {
+        scores.Sort((x, y) => x.score.CompareTo(y.score));
+    }
+
+    private void loadScores() {
+        if (PlayerPrefs.HasKey(PrefKeySize)) {
+            int size = PlayerPrefs.GetInt(PrefKeySize);
+
+            for (int i = 0; i < size; i++) {
+                string scoreString = PlayerPrefs.GetString(PrefKeyPrefix + i.ToString());
+                scores.Add(new ScoreData(scoreString));
+            }
+        }
+    }
+    private void fillDisplay() {
+        string displayText = "";
+        for (int i = 0; i < scores.Count; i++) {
+            displayText += scores[i].getFormatedScore() + " - " + scores[i].name;
+        }
+        display.text = displayText;
+    }
+}
